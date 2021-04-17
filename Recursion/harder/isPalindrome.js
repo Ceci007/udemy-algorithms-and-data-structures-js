c52e39dddd82b38cb3228a39f54a69febfedb1ad
Ceci007 <cecibenitezca@gmail.com>
2021-04-17 09:34:18 -0400

    Solved isPalindrome with recursion

---
+++ Recursion/harder/isPalindrome.js
@@ -0,0 +1,24 @@
+/*
+** Write a recursive function called isPalindrome which returns true if
+** the string passed to it is a palindrome (reads the same forward and backward).
+** Otherwise it returns false.
+*/
+
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
+
+const isPalindrome = (str) => {
+  if(str.length === 1) return true;
+  if(str.length === 2) return str[0] === str[1];
+  if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1));
+  return false;
+}
+
+console.log(isPalindrome('awesome')); // false
+console.log(isPalindrome('foobar')); // false
+console.log(isPalindrome('tacocat')); // true
+console.log(isPalindrome('amanaplanacanalpanama')); // true
+console.log(isPalindrome('amanaplanacanalpandemonium')); // false
