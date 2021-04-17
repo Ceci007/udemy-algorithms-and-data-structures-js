df2ac42339588a020fb3f006ca0fa2084792257e
Ceci007 <cecibenitezca@gmail.com>
2021-04-17 09:14:53 -0400

    Studing recursion, reverse a string challenge

---
+++ Recursion/harder/reverse.js
@@ -0,0 +1,17 @@
+/*
+** Write a recursive function called reverse which accepts a string and 
+** returns a new string in reverse.
+*/
+
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
+
+const reverse = (str) => {
+  if(str.length <= 1) return str;
+  return reverse(str.slice(1)) + str[0];
+}
+
+console.log(reverse('awesome'));
+console.log(reverse('rithmschool'));
+console.log(reverse('apple'));
+console.log(reverse('Swift'));
