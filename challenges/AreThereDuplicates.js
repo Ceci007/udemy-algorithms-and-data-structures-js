53b4ef1711cfa8f2f98534f7bc4380c3ef831168
Ceci007 <cecibenitezca@gmail.com>
2021-05-03 16:22:42 -0400

    Making code more compact

--- challenges/AreThereDuplicates.js
+++ challenges/AreThereDuplicates.js
@@ -5,16 +5,14 @@
 */
 
 const areThereDuplicates = (...args) => {
-  const argsArray = [...args];
-  const argsCounter = {};
+  const argsArr = [...args];
+  const argsCount = {};
 
-  for(let i = 0; i < argsArray.length; i++) {
-    argsCounter[argsArray[i]] = (argsCounter[argsArray[i]] || 0) + 1;
-  }
+  for(let i = 0; i < argsArr.length; i++) 
+    argsCount[argsArr[i]] = (argsCount[argsArr[i]] || 0) + 1;
 
-  for(let j = 0; j < argsArray.length; j++) {
-    if(argsCounter[argsArray[j]] > 1) return true;
-  }
+  for(let value in argsCount) 
+    if(argsCount[value] > 1) return true;
 
   return false;
 }
@@ -22,27 +20,3 @@ const areThereDuplicates = (...args) => {
 console.log([1, 2, 3], areThereDuplicates(1, 2, 3)); // false
 console.log([1, 2, 2], areThereDuplicates(1, 2, 2)); // true
 console.log(['a', 'b', 'c', 'a'], areThereDuplicates('a', 'b', 'c', 'a')); // true
-
-/*
-console.log(areThereDuplicates(1, 2, 3)); // false
-console.log(areThereDuplicates(1, 2, 2)); // true
-console.log(areThereDuplicates('a', 'b', 'c', 'a')); // true
-*/
-
-
-/*
-const areThereDuplicates = (...args) => {
-  const argsArr = [...args];
-  let collection = {};
-
-  for(let i = 0; i < argsArr.length; i++) {
-    collection[argsArr[i]] = (collection[argsArr[i]] || 0) + 1;
-  }
-
-  for(let value in collection) {
-    if(collection[value] > 1) return true;
-  }
-
-  return false;
-}
-*/
--- challenges/FrequencyCounter.js
+++ challenges/FrequencyCounter.js
@@ -4,63 +4,26 @@
 */
 
 const sameFrequency = (a, b) => {
-  const aStr = a.toString();
-  const bStr = b.toString();
-  const aCounter = {};
-  const bCounter = {};
+  const aDigits = a.toString();
+  const bDigits = b.toString();
+  const aCount = {};
+  const bCount = {};
 
-  if(aStr.length !== bStr.length) return false;
+  if(aDigits.length !== bDigits.length) return false;
 
-  for(let i = 0; i < aStr.length; i++) {
-    aCounter[aStr[i]] = (aCounter[aStr[i]] || 0) + 1;
-  }
+  for(let i = 0; i < aDigits.length; i++)
+    aCount[aDigits[i]] = (aCount[aDigits[i]] || 0) + 1;
 
-  for(let j = 0; j < aStr.length; j++) {
-    bCounter[bStr[j]] = (bCounter[bStr[j]] || 0) + 1;
-  }
+  for(let i = 0; i < bDigits.length; i++)
+    bCount[bDigits[i]] = (bCount[bDigits[i]] || 0) + 1;
 
-  for(let k in aCounter) {
-    if(aCounter[k] !== bCounter[k]) return false;
-  }
-
+  for(let value in aCount) 
+    if(aCount[value] !== bCount[value]) return false;
+    
   return true;
 }
-
 
 console.log(sameFrequency(182, 281)); // true
 console.log(sameFrequency(34, 14)); // false
 console.log(sameFrequency(3589578, 5879385)); // true
 console.log(sameFrequency(22, 222)); // false
-
-/*
-console.log(sameFrequency(182, 281)); // true
-console.log(sameFrequency(34, 14)); // false
-console.log(sameFrequency(3589578, 5879385)); // true
-console.log(sameFrequency(22, 222)); // false
-*/
-
-/*
-const sameFrequency = (num1, num2) => {
-  num1Str = num1.toString();
-  num2Str = num2.toString();
-
-  let collection1 = {};
-  let collection2 = {};
-
-  if(num1Str.length !== num2Str.length) return false;
-
-  for(let i = 0; i < num1Str.length; i++) {
-    collection1[num1Str[i]] = (collection1[num1Str[i]] || 0) + 1;
-  }
-
-  for(let j = 0; j < num2Str.length; j++) {
-    collection2[num2Str[j]] = (collection2[num2Str[j]] || 0) + 1;
-  }
-
-  for(let value in collection1) {
-    if(collection1[value] !== collection2[value]) return false;
-  }
-
-  return true;
-}
-*/
